37b39c020fc2c6906c2549a554813f00a7277f49
arnavmalhotra2025 <arnav.malhotra2025@gmail.com>
2025-08-20 16:35:02 +0200

    Add files via upload

---
+++ set homework.py
@@ -0,0 +1,8 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+symmetric_diff = set1.symmetric_difference(set2)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric Difference:", symmetric_diff)
